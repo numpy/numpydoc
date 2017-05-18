9af63b575c3b82205ecc63bc6118a69a4c1e9f24
Andreas Mueller <t3kcit@gmail.com>
2017-05-18 16:03:04 -0400

    add context to signature mangling errors

--- numpydoc/numpydoc.py
+++ numpydoc/numpydoc.py
@@ -113,8 +113,11 @@ def mangle_signature(app, what, name, obj, options, sig, retann):
 
     if not hasattr(obj, '__doc__'):
         return
-
-    doc = SphinxDocString(pydoc.getdoc(obj))
+    try:
+        doc = SphinxDocString(pydoc.getdoc(obj))
+    except ValueError as e:
+        filename = obj.__code__.co_filename
+        raise ValueError(str(e) + " in {}:{}".format(filename, name))
     sig = doc['Signature'] or getattr(obj, '__text_signature__', None)
     if sig:
         sig = re.sub(sixu("^[^(]*"), sixu(""), sig)
