2b6732519ec6931d6eb33194744d9aeee85fabde
Joel Nothman <joel.nothman@gmail.com>
2017-11-13 18:34:06 +1100

    Automatically load autosummary
    Fix #141

--- numpydoc/numpydoc.py
+++ numpydoc/numpydoc.py
@@ -151,6 +151,8 @@ def setup(app, get_doc_object_=get_doc_object):
     app.add_domain(NumpyPythonDomain)
     app.add_domain(NumpyCDomain)
 
+    app.setup_extension('sphinx.ext.autosummary')
+
     metadata = {'version': __version__,
                 'parallel_read_safe': True}
     return metadata
