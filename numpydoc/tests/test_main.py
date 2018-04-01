91a78dfa36015f77a84073f100caa01bff097c75
Joel Nothman <joel.nothman@gmail.com>
2018-04-01 22:38:57 +1000

    Use StringIO.StringIO in Py2

--- numpydoc/tests/test_main.py
+++ numpydoc/tests/test_main.py
@@ -1,11 +1,14 @@
 from numpydoc.__main__ import main
 
 import sys
-import io
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 
 def test_main():
-    f = io.StringIO()
+    f = StringIO()
     sys.stdout, old_stdout = f, sys.stdout
     main(['numpydoc.__main__.main'])
     assert f.getvalue().strip() == main.__doc__
