5bc03b69f37e98625686185197a91edee899d3b2
Eric Larson <larson.eric.d@gmail.com>
2020-05-26 10:50:59 -0400

    MAINT: Bump to 1.1.0.dev0 (#266)

--- numpydoc/__init__.py
+++ numpydoc/__init__.py
@@ -2,7 +2,7 @@
 This package provides the numpydoc Sphinx extension for handling docstrings
 formatted according to the NumPy documentation format.
 """
-__version__ = '1.0.0'
+__version__ = '1.1.0.dev0'
 
 
 def setup(app, *args, **kwargs):
