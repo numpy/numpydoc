4e7c5ad998de88cea965dbfb5c86da5dffb0f806
Eric Larson <larson.eric.d@gmail.com>
2020-07-01 17:59:23 -0400

    MAINT: Tick version

--- numpydoc/__init__.py
+++ numpydoc/__init__.py
@@ -2,7 +2,7 @@
 This package provides the numpydoc Sphinx extension for handling docstrings
 formatted according to the NumPy documentation format.
 """
-__version__ = '1.1.0'
+__version__ = '1.2.dev0'
 
 
 def setup(app, *args, **kwargs):
