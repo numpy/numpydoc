2033375ea1ae474f3c22c9b76733df0122bd896f
Ross Barnowski <rossbar@berkeley.edu>
2020-12-08 21:59:39 -0800

    Improve _error_location method output.

--- numpydoc/docscrape.py
+++ numpydoc/docscrape.py
@@ -418,8 +418,8 @@ class NumpyDocString(Mapping):
                 filename = inspect.getsourcefile(self._obj)
             except TypeError:
                 filename = None
-            msg = msg + (" in the docstring of %s in %s."
-                         % (self._obj, filename))
+            msg += f" in the docstring of {self._obj.__name__}"
+            msg += f" in {filename}." if filename else ""
         if error:
             raise ValueError(msg)
         else:
