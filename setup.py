5b3b4a4abe7fd46c8f647d17ab7e6adc091d8b9c
Eric Larson <larson.eric.d@gmail.com>
2021-04-21 11:13:29 -0400

    FIX: Fix RTD

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ setup(
                  "Programming Language :: Python :: 3.5",
                  "Programming Language :: Python :: 3.6",
                  "Programming Language :: Python :: 3.7",
+                 "Programming Language :: Python :: 3.8",
+                 "Programming Language :: Python :: 3.9",
                  ],
     keywords="sphinx numpy",
     author="Pauli Virtanen and others",
