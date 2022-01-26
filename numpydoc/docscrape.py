32df982bc2fd4770ba00a5f7e6f0b0b6cd093645
Ross Barnowski <rossbar@berkeley.edu>
2022-01-25 17:44:46 -0800

    Add test for bad behavior.

--- numpydoc/docscrape.py
+++ numpydoc/docscrape.py
@@ -426,7 +426,12 @@ class NumpyDocString(Mapping):
                 filename = inspect.getsourcefile(self._obj)
             except TypeError:
                 filename = None
-            msg += f" in the docstring of {self._obj.__name__}"
+            # Make UserWarning more descriptive via object introspection.
+            # Skip if introspection fails
+            try:
+                msg += f" in the docstring of {self._obj.__name__}"
+            except AttributeError:
+                pass
             msg += f" in {filename}." if filename else ""
         if error:
             raise ValueError(msg)
