4aea3d22dd9ffaad6258594fa064caf267624951
Ross Barnowski <rossbar@berkeley.edu>
2022-04-13 11:18:01 -0700

    Runtime verification of sphinx and jinja2 versions (#381)
    * Add runtime sphinx/jinja version checker.
    
    * Remove jinja2 pin.
    
    * modify CI to account for removed pin.

--- numpydoc/__init__.py
+++ numpydoc/__init__.py
@@ -5,6 +5,31 @@ formatted according to the NumPy documentation format.
 __version__ = '1.2.2.dev0'
 
 
+def _verify_sphinx_jinja():
+    """Ensure sphinx and jinja versions are compatible.
+
+    Jinja2>=3.1 requires Sphinx>=4.0.2. Raises exception if this condition is
+    not met.
+
+    TODO: This check can be removed when the minimum supported sphinx version
+    for numpydoc sphinx>=4.0.2
+    """
+    import sphinx, jinja2
+    from packaging import version
+
+    if version.parse(sphinx.__version__) <= version.parse("4.0.2"):
+        if version.parse(jinja2.__version__) >= version.parse("3.1"):
+            from sphinx.errors import VersionRequirementError
+            raise VersionRequirementError(
+                "\n\nSphinx<4.0.2 is incompatible with Jinja2>=3.1.\n"
+                "If you wish to continue using sphinx<4.0.2 you need to pin "
+                "Jinja2<3.1."
+            )
+
+
+_verify_sphinx_jinja()
+
+
 def setup(app, *args, **kwargs):
     from .numpydoc import setup
     return setup(app, *args, **kwargs)
