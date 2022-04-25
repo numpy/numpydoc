eb22997831c36656907fac11e808b0c01c15aecc
Jarrod Millman <jarrod.millman@gmail.com>
2022-04-25 12:56:03 -0700

    Require sphinx>3 (#385)

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     author_email="pav@iki.fi",
     url="https://numpydoc.readthedocs.io",
     license="BSD",
-    install_requires=["sphinx>=1.8", 'Jinja2>=2.10'],
+    install_requires=["sphinx>=3.0", 'Jinja2>=2.10'],
     python_requires=">=3.7",
     extras_require={
         "testing": [
