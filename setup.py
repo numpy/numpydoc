6e443a778fb4a5e8ba646cf172e68a127fadf5e7
Jarrod Millman <jarrod.millman@gmail.com>
2022-05-27 13:29:00 -0700

    Fix bug with version name
    This is likely caused by black, but is a simple fix.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ version = None
 with open(os.path.join("numpydoc", "_version.py")) as fid:
     for line in (line.strip() for line in fid):
         if line.startswith("__version__"):
-            version = line.split("=")[1].strip().strip("'")
+            version = line.split("=")[1].strip().strip('"')
             break
 if version is None:
     raise RuntimeError("Could not determine version")
