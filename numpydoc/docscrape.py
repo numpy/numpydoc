ddcf0fea7a68d9f826ed863588ecc494b11d83a4
Ross Barnowski <rossbar@berkeley.edu>
2022-09-28 11:21:38 -0700

    Add cached property support (#433)
    * TST: Add test case for cached_property.
    
    * BUG: Fix detection of cached_property attrs.
    
    Co-authored-by: Tirth Patel <tirthasheshpatel@gmail.com>
    
    * Wrap cached_property import in try/except for Python 3.7.
    
    Co-authored-by: Tirth Patel <tirthasheshpatel@gmail.com>

--- numpydoc/docscrape.py
+++ numpydoc/docscrape.py
@@ -12,6 +12,13 @@ import copy
 import sys
 
 
+# TODO: Remove try-except when support for Python 3.7 is dropped
+try:
+    from functools import cached_property
+except ImportError:  # cached_property added in Python 3.8
+    cached_property = property
+
+
 def strip_blank_lines(l):
     "Remove leading and trailing blank lines from a list of lines"
     while l and not l[0].strip():
@@ -706,7 +713,7 @@ class ClassDoc(NumpyDocString):
                 not name.startswith("_")
                 and (
                     func is None
-                    or isinstance(func, property)
+                    or isinstance(func, (property, cached_property))
                     or inspect.isdatadescriptor(func)
                 )
                 and self._is_show_member(name)
