9d0bab831f5f4dd73048b48afe5cb2d4f1ff194e
Ross Barnowski <rossbar@berkeley.edu>
2023-01-23 23:05:20 -0800

    STY: Run black.

--- numpydoc/validate.py
+++ numpydoc/validate.py
@@ -584,8 +584,7 @@ def validate(obj_name):
                     ("string", "str"),
                 ]
                 for wrong_type, right_type in common_type_errors:
-                    if wrong_type in set(re.split(r"\W", doc.parameter_type(
-                            param))):
+                    if wrong_type in set(re.split(r"\W", doc.parameter_type(param))):
                         errs.append(
                             error(
                                 "PR06",
