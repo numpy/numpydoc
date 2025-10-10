d88ae338c5ce57656c15707ffbcdf61b9db7915b
Eric Larson <larson.eric.d@gmail.com>
2025-10-10 09:41:53 -0400

    MAINT: Make pre-commit run --all pass

--- numpydoc/tests/test_validate.py
+++ numpydoc/tests/test_validate.py
@@ -1391,11 +1391,8 @@ class TestValidator:
         ],
     )
     def test_bad_generic_functions(self, capsys, func):
-        with pytest.warns(UserWarning):
-            errors = validate_one(
-                self._import_path(klass="WarnGenericFormat", func=func)
-            )
-        assert "is too short" in w.msg
+        with pytest.warns(UserWarning, match="is too short"):
+            validate_one(self._import_path(klass="WarnGenericFormat", func=func))
 
     @pytest.mark.parametrize(
         "func",
